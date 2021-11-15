9c60e4d7d2192b751708f0ae75dc32143e0c8939
Katharina Fey <kookie@spacekookie.de>
2021-11-15 13:39:10 +0100

    ignore me

--- implementations/rust/ockam/ockam/src/pipe/receiver.rs
+++ implementations/rust/ockam/ockam/src/pipe/receiver.rs
@@ -1,1 +1,26 @@
+use ockam_core::{Address, Any, Result, Routed, Worker};
+use ockam_node::Context;
+
 pub struct PipeReceiver {}
+
+#[crate::worker]
+impl Worker for PipeReceiver {
+    type Context = Context;
+    type Message = Any;
+
+    async fn initialize(&mut self, ctx: &mut Context) -> Result<()> {
+        ctx.set_cluster(super::CLUSTER_NAME).await?;
+        Ok(())
+    }
+
+    async fn handle_message(&mut self, _: &mut Context, msg: Routed<Any>) -> Result<()> {
+        info!("Pipe receiver: {:?}", msg);
+        Ok(())
+    }
+}
+
+impl PipeReceiver {
+    pub async fn create(ctx: &mut Context, addr: Address) -> Result<()> {
+        ctx.start_worker(addr, PipeReceiver {}).await
+    }
+}
